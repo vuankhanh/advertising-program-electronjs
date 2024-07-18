7a824e5e0a255680c3710c29e3eecd7326f8e6dd
VU AN KHANH <vuankhanh19071992@gmail.com>
2024-07-18 13:04:23 +0700

    git commit -m "first commit"

---
+++ src/index.js
@@ -0,0 +1,77 @@
+const { app, BrowserWindow, globalShortcut } = require('electron');
+const path = require('node:path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    fullscreen: true,
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, '../dist/digital-signage-angular/browser/index.html'));
+
+  // Remove the menu bar
+  mainWindow.setMenu(null);
+
+  // Open the DevTools.
+  if (process.env.NODE_ENV !== 'production') {
+    mainWindow.webContents.openDevTools();
+  }
+
+  return mainWindow;
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  const mainWindow = createWindow();
+
+  // Register global shortcuts
+  globalShortcut.register('F11', () => {
+    if (mainWindow.isFullScreen()) {
+      mainWindow.setFullScreen(false);
+    } else {
+      mainWindow.setFullScreen(true);
+    }
+  });
+
+  globalShortcut.register('Esc', () => {
+    mainWindow.minimize();
+  });
+
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Unregister all global shortcuts when the app is about to quit
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
